3d1a94fa5e2fa9bee65d9b2de9fb5c2926584845
00010590wiut <00010590wiut@gmail.com>
2020-12-08 19:50:11 +0500

    completed the homepage

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+    const burger_menu = document.querySelector('.mobile-burger-menu');
+    const nav = document.querySelector('.menu-links');
+    const navLinks = document.querySelectorAll('.menu-links li');
+   
+    burger_menu.addEventListener('click',() => {
+        //Toggle nav
+        nav.classList.toggle('nav-active');
+
+         //animate links
+        navLinks.forEach((link, index) => {
+        if (link.style.animation){
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+        }
+    }); 
+    //Burger_menu animation
+    burger_menu.classList.toggle('link_animation');
+});
+    
+}
+
+navSlide();
