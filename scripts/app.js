4f00e97d60fd83e8440742267a306bfea7b7c828
00010590wiut <00010590wiut@gmail.com>
2020-12-10 17:02:03 +0500

    some responsivness

--- scripts/app.js
+++
@@ -1,8 +0,0 @@
-//Select eleents from DOM
-const searchButton = document.querySelector('#search');
-const inputElement = document.querySelector('#inputValue');
-
-buttonElement.onclick = function(event) {
-    event.preventDefault();
-    const value = inputElement.value;
-}
