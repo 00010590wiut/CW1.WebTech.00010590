a8436f97db4da705efe386d16e4aa842c6ac6791
00010590wiut <00010590wiut@gmail.com>
2020-12-10 13:22:17 +0500

    discover page added

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+//Select eleents from DOM
+const searchButton = document.querySelector('#search');
+const inputElement = document.querySelector('#inputValue');
+
+buttonElement.onclick = function(event) {
+    event.preventDefault();
+    const value = inputElement.value;
+}
